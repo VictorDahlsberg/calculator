b5b8725a5b2a30a4a9be5adc047299f54bc93b7d
Victor <victordahlsberg@hotmail.com>
2023-02-10 11:11:01 +0100

    Code is working with tests from definiton, time to format

--- calculator.py
+++ calculator.py
@@ -1,32 +1,75 @@
 class Register:
-    def __init__(self, operation, value):
+    def __init__(self, operation, operand):
+        self.value: int = 0
+        self.expression: list[list[str, str]] = [[operation, operand]]
+        self.is_evaluated = False
+
+    def reset_register(self):
         self.value = 0
-        self.expression = []
+        self.is_evaluated = False
 
 
 class Calculator:
     def __init__(self):
-        self.registers: dict[Register] = {}
+        self.registers: dict[str, Register] = {}
 
-    def add(self, register, value):
-        if not self.registers[register]:
-            self.registers[register] = Register("add", value)
+    def add(self, register, operand):
+        if register in self.registers:
+            self.registers[register].expression.append(["add", operand])
+            self.registers[register].reset_register()
         else:
-            self.registers[register].expression.append("add", value)
+            self.registers[register] = Register("add", operand)
 
-    def subtract(self, register: str, value):
-        pass
+    def subtract(self, register: str, operand):
+        if register in self.registers:
+            self.registers[register].expression.append(["subtract", operand])
+            self.registers[register].reset_register()
+        else:
+            self.registers[register] = Register("subtract", operand)
 
-    def multiply(self, register: str, value):
-        pass
+    def multiply(self, register: str, operand):
+        if register in self.registers:
+            self.registers[register].expression.append(["multiply", operand])
+            self.registers[register].reset_register()
+        else:
+            self.registers[register] = Register("multiply", operand)
 
-    def evaluate_register(self, register):
-        pass
+    # TODO: Argumetn should be string
+    def evaluate_register(self, register: Register):
+        if register.is_evaluated:
+            return register.value
+
+        for operation, operand in register.expression:
+            if operation == "add":
+                if operand.isnumeric():
+                    register.value += int(operand)
+
+                else:
+                    register.value += self.evaluate_register(
+                        self.registers[operand])
+
+            elif operation == "subtract":
+                if operand.isnumeric():
+                    register.value -= int(operand)
+
+                else:
+                    register.value -= self.evaluate_register(
+                        self.registers[operand])
+
+            elif operation == "multiply":
+                if operand.isnumeric():
+                    register.value *= int(operand)
+
+                else:
+                    register.value *= self.evaluate_register(
+                        self.registers[operand])
+
+        register.is_evaluated = True
+        return register.value
 
 
 class CalculatorInterface:
     VALID_OPERATIONS = ["add", "subtract", "multiply"]
-    self.registers: dict[binaryOperation]
 
     def __init__(self) -> None:
         self.calculator: Calculator = Calculator()
@@ -42,7 +85,9 @@ class CalculatorInterface:
             case["print", register]:
                 # TODO: check if register exists
                 if self.is_valid_register(register):
-                    print(self.calculator.evaluate_register(register))
+                    self.calculator.registers[register].reset_register()
+                    print(self.calculator.evaluate_register(
+                        self.calculator.registers[register]))
 
             case[register, operation, value]:
                 if not self.is_valid_register(register) or not self.is_valid_value(value):
@@ -56,10 +101,10 @@ class CalculatorInterface:
                     self.calculator.add(register, value)
 
                 elif operation == "subtract":
-                    self.calculator.add(register, value)
+                    self.calculator.subtract(register, value)
 
                 elif operation == "multiply":
-                    self.calculator.add(register, value)
+                    self.calculator.multiply(register, value)
 
             case _:
                 print("Invalid command")
